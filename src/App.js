88c13b3162f26e97ad2e70533a56a101e546dc0d
#definenithish <100853986+Definenithish@users.noreply.github.com>
2024-11-25 07:51:02 +0000

    registrationpage

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 // import About from "./components/About";
 import Contactus from "./components/Contactus";
+import RegistrationPage from "./components/Registration";
+
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
         <Route path="/navbar" element={<Navbar />} />
         {/* <Route path="/about" element={<About />} /> */}
         <Route path="/contact" element={<Contactus />} />
+        <Route path="/Registration" element={<RegistrationPage />} />
       </Routes>
     </BrowserRouter>
   );
